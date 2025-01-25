6c9b138aaaa402b4c78d085164ada5c6aa6c5908
Shivam <kumarshivam3788@gmail.com>
2025-01-25 12:55:10 +0530

    feat: add PostgreSQL and Redis setup in Docker and update dependencies

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,13 @@
-import http from 'http'
-import app  from './app/server'
+import http from "http";
+import app from "./app/server";
+import pg from "pg";
+import {Redis} from "ioredis";
 
-async function init(){
-    try {
+async function init() {
+  try {
     // Redis Connection
     console.log(`Connecting Redis...`);
-    const redis = new Redis("redis://redis:6379", { lazyConnect: true });
+    const redis = new Redis("redis://localhost:6379", { lazyConnect: true });
     await redis.connect();
     console.log(`Redis Connection Success...`);
 
@@ -14,21 +16,23 @@ async function init(){
 
     const { Client } = pg;
     const client = new Client({
-      host: "db",
-      port: 5432,
+      host: "localhost",
+      port: 5431,
       database: "postgres",
       user: "postgres",
       password: "postgres",
     });
     await client.connect();
-        const PORT = process.env.PORT || 8000
-        const server = http.createServer(app)
-        server.listen(PORT, () => {
-            console.log(`Http server is listening on PORT ${PORT}`)
-        })
-    } catch (error) {
-        console.log(`Error Starting Server`, error)
-    }
+    console.log(`Postgres Connection Success...`);
+
+    const PORT = process.env.PORT || 8000;
+    const server = http.createServer(app);
+    server.listen(PORT, () => {
+      console.log(`Http server is listening on PORT ${PORT}`);
+    });
+  } catch (error) {
+    console.log(`Error Starting Server`, error);
+  }
 }
 
 init();
